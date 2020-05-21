8f98df3f0f1a53ec4c2726d66f0b185aded276ff
Deborah Alkmin <deborah_alkmin@hotmail.com>
2020-05-20 23:19:44 -0300

    Adiciona relatorios

--- src/br/com/modelo/Caixa.java
+++ src/br/com/modelo/Caixa.java
@@ -1,7 +1,11 @@
 package br.com.modelo;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 import br.com.negocio.Menu;
@@ -94,6 +98,107 @@ public class Caixa {
 			System.out.println("Tipo: " + entrada.getGrupo());
 		}
 	}
+	
+	//Relatório mensal
+	public void relatorioMensal() {
+		user = login.userLogado();
+		System.out.println("Digite o mês e o ano do relatório: (mm/aaaa)");
+		String d = scanner.nextLine();
+		int m = Integer.parseInt(d.substring(0,2));
+		int a = Integer.parseInt(d.substring(3));
+		double totalPMensal = 0;
+		double totalNMensal = 0;
+		System.out.println("|--------RELATÓRIO MENSAL--------|");
+		System.out.println("----ENTRADA----");
+		for (Entrada entrada : entradas) {
+			if(entrada.getUsuario().equals(user)) {
+				if((entrada.getData().getMonth()+1 == m)&&(entrada.getData().getYear()+1900 == a)){
+					if(user.getTipo() == 1)
+						System.out.println(grupoEPFisica[entrada.getGrupo()]+ ": " +"R$"+entrada.getValor());
+					else
+						System.out.println(grupoEPJuridica[entrada.getGrupo()]+ ": " +"R$"+entrada.getValor());
+					System.out.println("Data: " + f.format(entrada.getData())+ "\n");
+					totalPMensal+=entrada.getValor();
+				}
+			}	
+		}
+		
+		System.out.println("\n----SAIDA----");
+		for (Saida saida: saidas) {
+			if(saida.getUsuario().equals(user)){
+				if((saida.getData().getMonth()+1 == m)&&(saida.getData().getYear()+1900 == a)){
+					if(user.getTipo() == 1)
+						System.out.println(grupoSPFisica[saida.getGrupo()]+ ": " +"R$"+saida.getValor());
+					else
+						System.out.println(grupoSPJuridica[saida.getGrupo()]+ ": " +"R$"+saida.getValor());
+					System.out.println("Data: " + f.format(saida.getData())+ "\n");
+					totalNMensal+=saida.getValor();
+				}
+			}
+		}
+
+		System.out.println("\nFluxo do mês: R$"+(totalPMensal-totalNMensal));
+		
+	}
+	
+	//Relatório semanal
+	public void relatorioSemanal() {
+		user = login.userLogado();
+		Calendar calendar = new GregorianCalendar();
+		Calendar calendar2 = new GregorianCalendar();
+		//Date trialTime = new Date();   
+		
+		System.out.println("Digite a data que deseja receber o relatório: (dd/mm/aaaa)");
+		String d = scanner.nextLine();
+		Date dataFormatada = null;
+		try {
+			dataFormatada = f.parse(d);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		calendar.setTime(dataFormatada);     
+		int semana = calendar.get(Calendar.WEEK_OF_YEAR); 
+		int semana2;
+		
+		double totalPSemanal= 0;
+		double totalNSemanal = 0;
+		System.out.println("|--------RELATÓRIO SEMANAL--------|");
+		System.out.println("----ENTRADA----");
+		for (Entrada entrada : entradas) {
+			if(entrada.getUsuario().equals(user)) {
+				calendar2.setTime(entrada.getData());
+				semana2 = calendar2.get(Calendar.WEEK_OF_YEAR);
+				if(semana == semana2) {
+					if(user.getTipo() == 1)
+						System.out.println(grupoEPFisica[entrada.getGrupo()]+ ": " +"R$"+entrada.getValor());
+					else
+						System.out.println(grupoEPJuridica[entrada.getGrupo()]+ ": " +"R$"+entrada.getValor());
+					System.out.println("Data: "+f.format(entrada.getData())+"\n");
+					totalPSemanal+=entrada.getValor();
+				}
+			}
+		}
+		
+		System.out.println("\n----SAIDA----");
+		for (Saida saida : saidas) {
+			if(saida.getUsuario().equals(user)) {
+				calendar2.setTime(saida.getData());
+				semana2 = calendar2.get(Calendar.WEEK_OF_YEAR);
+				if(semana == semana2) {
+					if(user.getTipo() == 1)
+						System.out.println(grupoSPFisica[saida.getGrupo()]+ ": " +"R$"+saida.getValor());
+					else
+						System.out.println(grupoSPJuridica[saida.getGrupo()]+ ": " +"R$"+saida.getValor());
+					System.out.println("Data: "+f.format(saida.getData())+"\n");
+					totalNSemanal+=saida.getValor();
+				}
+			}
+		}
+		
+		System.out.println("\nFluxo da semana: R$"+(totalPSemanal-totalNSemanal));
+	}
+		
 	
 	public void menuPrincipal() {
 		Menu menu = new Menu();
